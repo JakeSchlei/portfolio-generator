2557013b3f601ba47bc908c48b65e1845080b9e0
JakeSchlei <jakeschlei@gmail.com>
2022-09-07 15:10:39 -0700

    generated all of the data from questions to html sheet

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
@@ -138,8 +138,14 @@ const promptProject = portfolioData => {
 
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
+  .then(promptProject)
+  .then((portfolioData) => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log("Portfolio complete! Check out index.html to see the output!");
     });
+  });
 
